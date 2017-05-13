64817c2642fdff5a0e88510ee953c09e35cda5df
rathaiah45 <yasamrathaiah@gmail.com>
2017-05-13 08:25:40 +0530

    firstcommit

---
+++ SpringMvcExample/src/main/java/com/cts/form/Employee.java
@@ -0,0 +1,12 @@
+package com.cts.form;
+
+import java.io.Serializable;
+import java.util.HashSet;
+public class Employee implements Serializable{
+HashSet list=null;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
